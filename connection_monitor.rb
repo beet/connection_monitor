dfea3d0f54f5f732edbff6969740af61c90923ad
Mark Beattie <beattie.mark@gmail.com>
2020-07-30 14:10:52 +1000

    Now continuously polls the connection and notifies whenever it drops in/out.

--- connection_monitor.rb
+++ connection_monitor.rb
@@ -1,38 +1,70 @@
 require 'net/http'
+require "ostruct"
 
 class ConnectionMonitor
-  attr_reader :attempts
+  POLLING_INTERVAL = 3
+  CONNECTION_STATUSES = OpenStruct.new(online: 1, offline: 0)
+
+  attr_reader :attempts, :outages, :connection_status
 
   def initialize
     @attempts = 0
+    @outages = 0
+    @connection_status = nil
   end
 
   def start
-    while internet_down? do
-      print "."
+    while true
+      current_connection_status = get_connection_status
 
-      sleep 10
+      if current_connection_status != connection_status
+        if current_connection_status == CONNECTION_STATUSES.online
+          @attempts = 0
+        end
+
+        if current_connection_status == CONNECTION_STATUSES.offline
+          @outages += 1
+        end
+
+        @connection_status = current_connection_status
+
+        `say "Internet connection #{connection_status_string}"`
+      end
+
+      print online? ? "." : "X"
+
+      sleep POLLING_INTERVAL
     end
+  rescue Interrupt => exception
+    puts "\n\nInternet connection experienced #{outages} outages"
+  end
 
-    puts "\n\nInternet back online after #{@attempts} attempts!"
+  def connection_status_string
+    online? ? "online" : "off-line"
+  end
 
-    `say "Internet is back online"`
+  def online?
+    connection_status == CONNECTION_STATUSES.online
+  end
+
+  def offline?
+    connection_status == CONNECTION_STATUSES.offline
   end
 
   private
 
-  def internet_down?
+  def get_connection_status
     begin
-      return false if Net::HTTP.get(uri)
+      return CONNECTION_STATUSES.online if Net::HTTP.get(uri)
     rescue SocketError => e
       @attempts += 1
 
-      return true
+      return CONNECTION_STATUSES.offline
     end
   end
 
   def uri
-    URI("https://google.com")
+    URI("https://www.google.com")
   end
 end
 
