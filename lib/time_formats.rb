1f5b0985afab6a5e9d851b836454757fd76fbde6
Mark Beattie <beattie.mark@gmail.com>
2021-03-23 09:10:00 +1100

    Show date in outage timestamps

--- lib/time_formats.rb
+++ lib/time_formats.rb
@@ -1,14 +1,14 @@
 module TimeFormats
   refine Time do
     def to_time_string
-      self.strftime('%R:%S')
+      self.strftime('%Y-%m-%d %R:%S')
     end
   end
 
   [Integer, Float].each do |klass|
     refine klass do
       def to_duration_string
-        Time.at(self).gmtime.to_time_string
+        Time.at(self).gmtime.strftime("%R:%S")
       end
     end
   end
